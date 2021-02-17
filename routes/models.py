2dbd0e806f5d47c312859b16ae067449fe969c3d
MaxKayan <maxim.kayander1@gmail.com>
2021-02-18 00:37:41 +0300

    Added Destination and DestinationPhoto models

--- routes/models.py
+++ routes/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from routes.choices import DESTINATION_TYPES
+
 
 class TimestampModelMixin:
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Создано")
@@ -7,6 +9,11 @@ class TimestampModelMixin:
 
     # class Meta:
     #     abstract = True
+
+
+class GeoPointMixin:
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
 
 
 class Route(models.Model, TimestampModelMixin):
@@ -33,3 +40,22 @@ class Waypoint(models.Model, TimestampModelMixin):
         constraints: [
             models.UniqueConstraint(fields=["route", "longitude", "latitude"], name="unique_waypoint")
         ]
+
+
+class Destination(models.Model, TimestampModelMixin, GeoPointMixin):
+    title = models.CharField(verbose_name="Название", max_length=64)
+    type = models.CharField(verbose_name="Тип", max_length=16, choices=DESTINATION_TYPES)
+    description = models.TextField(verbose_name="Описание")
+
+    class Meta:
+        verbose_name: "Пункт назначения"
+        verbose_name_plural: "Пункты назначения"
+
+
+class DestinationPhoto(models.Model, TimestampModelMixin):
+    destination = models.ForeignKey(Destination, on_delete=models.CASCADE, related_name="photos")
+    image = models.ImageField(verbose_name="Фотография", upload_to="destination_photos/")
+
+    class Meta:
+        verbose_name: "Фотография точки"
+        verbose_name_plural: "Фотографии точек"
