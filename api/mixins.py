e4e2175bdfb61ed8d0675d4c6a3b1f30ef5dac69
MaxKayan <maxim.kayander1@gmail.com>
2021-02-20 15:59:10 +0300

    Added action serializer mixin

---
+++ api/mixins.py
@@ -0,0 +1,8 @@
+class ActionSerializerMixin(object):
+    # serializer_action_classes = {}
+
+    def get_serializer_class(self):
+        try:
+            return self.serializer_action_classes[self.action]
+        except (KeyError, AttributeError):
+            return super().get_serializer_class()
