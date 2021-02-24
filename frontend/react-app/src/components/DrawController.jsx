b60a6ba33920d77080194117e0f8db52759e77ec
MaxKayan <maxim.kayander1@gmail.com>
2021-02-24 23:05:27 +0300

    Working path drawing with polyline display. Also a simple total distance calculation test.

--- frontend/react-app/src/components/DrawController.jsx
+++ frontend/react-app/src/components/DrawController.jsx
@@ -1,18 +1,24 @@
-import {Polyline, useMapEvents} from "react-leaflet";
+import {CircleMarker, Polyline, useMapEvents} from "react-leaflet";
+import {useGeoPoints} from "../hooks/useGeoPoints";
 
 export const DrawController = () => {
+    const {points, addPoint, getTotalDistance} = useGeoPoints([]);
 
     const map = useMapEvents({
         click(e) {
             console.log(e);
+            addPoint([e.latlng.lat, e.latlng.lng]);
         },
     });
 
-    const increment = 0.001;
+    const color = "red";
 
-
+    console.log(points, getTotalDistance());
     return (
-        <Polyline positions={path}>
-        </Polyline>
+        <>
+            {points.length > 0 && <CircleMarker center={points[0]} radius={2} color={color}/>}
+            <Polyline positions={points} color={color}/>
+            {points.length > 1 && <CircleMarker center={points[points.length - 1]} radius={2} color={color}/>}
+        </>
     );
 };
--- frontend/react-app/src/hooks/useGeoPoints.js
+++ frontend/react-app/src/hooks/useGeoPoints.js
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import {getDistanceFromLatLonInKm} from "../maths/haversine";
 
 export const useGeoPoints = (initialValue) => {
     const [points, editPoints] = useState([]);
@@ -6,4 +7,26 @@ export const useGeoPoints = (initialValue) => {
     const addPoint = (point) => {
         editPoints([...points, point]);
     };
+
+    const removePoint = (point) => {
+        const newList = points.filter(value => value === point);
+        editPoints(newList);
+    };
+
+    const getPoint = (index) => points.length > index ? points[index] : null;
+
+    const getTotalDistance = () => {
+        const distance = points.reduce((prev, current, i) => {
+            if (i > 0) {
+                console.log(points[i - 1], points[i]);
+                return getDistanceFromLatLonInKm(points[i - 1], points[i]);
+            }
+            return 0;
+        }, 0.0);
+
+        console.log(distance);
+        return distance;
+    };
+
+    return {points, addPoint, removePoint, getPoint, getTotalDistance};
 };
