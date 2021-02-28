e40915591a53ea7c727da0d9cb496b949737e992
MaxKayan <maxim.kayander1@gmail.com>
2021-02-28 23:26:02 +0300

    Implemented logic for the dynamic cursor mode based on the map interactions

--- frontend/react-app-ts/src/components/MapView/MapView.tsx
+++ frontend/react-app-ts/src/components/MapView/MapView.tsx
@@ -1,11 +1,20 @@
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import {LocationMarker} from "../LocationMarker/LocationMarker";
 import {DrawController} from "../DrawController/DrawController";
-import React from "react";
-import {LatLng} from "leaflet";
+import React, {useEffect, useState} from "react";
+import {LatLng, LeafletEventHandlerFnMap} from "leaflet";
 
 import "./MapView.css";
 import styled from "styled-components";
+import {MapEventsController} from "../MapEventsController/MapEventsController";
+
+
+enum CursorMode {
+    Default,
+    Drawing,
+    Dragging
+}
+
 
 type MapViewProps = {
     startPosition: LatLng,
@@ -14,21 +23,48 @@ type MapViewProps = {
     dragging?: boolean
 }
 
+
 type OverlayProps = {
-    drawCursor: boolean
+    mode: CursorMode
 }
+
 
 const Overlay = styled.div<OverlayProps>`
   & > div {
-    ${({drawCursor}) => drawCursor ?
-            "cursor: pointer !important" : "cursor: grab"
+    ${({mode}) => {
+    switch (mode) {
+        case CursorMode.Drawing:
+            return "cursor: pointer !important";
+
+        case CursorMode.Dragging:
+            return "cursor: grabbing !important";
+
+        default:
+            return "";
     }
-  }
-`;
+}}`;
+
 
 export const MapView: React.FC<MapViewProps> = ({startPosition, defaultZoom, drawEnabled}) => {
+    const getCursorMode = (drawing: boolean = drawEnabled): CursorMode => {
+        return drawing ? CursorMode.Drawing : CursorMode.Default;
+    };
+
+    const [cursorMode, editCursorMode] = useState<CursorMode>(CursorMode.Default);
+
+    useEffect(() => editCursorMode(getCursorMode()), [drawEnabled]); //TODO: Inspect the cause of this EsLint warning
+
+    const handlers: LeafletEventHandlerFnMap = {
+        dragstart() {
+            editCursorMode(CursorMode.Dragging);
+        },
+        dragend() {
+            editCursorMode(getCursorMode());
+        }
+    };
+
     return (
-        <Overlay drawCursor={drawEnabled}>
+        <Overlay mode={cursorMode}>
             <MapContainer className={"main-map"} center={startPosition} zoom={defaultZoom} scrollWheelZoom={true}>
                 <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -42,6 +78,8 @@ export const MapView: React.FC<MapViewProps> = ({startPosition, defaultZoom, dra
                 </Marker>
                 <LocationMarker/>
                 <DrawController enabled={drawEnabled}/>
+
+                <MapEventsController handlers={handlers}/>
             </MapContainer>
         </Overlay>
     );
