d9ec2c0d0ad6d2d572c3a098f22fc8c7538e114e
MaxKayan <maxim.kayander1@gmail.com>
2021-03-02 17:00:36 +0300

    Using FA icons. Style changes

--- frontend/react-app-ts/src/components/ToggleButton/ToolButton.tsx
+++ frontend/react-app-ts/src/components/ToggleButton/ToolButton.tsx
@@ -1,15 +1,10 @@
 import React from "react";
 import styled from "styled-components";
+import classNames from "classnames";
 
-type ToggleButtonProps = {
-    label: string,
-    iconUrl: string,
-    active: boolean,
-    callback: () => void
-}
 
 type StyleProps = {
-    active: boolean
+    active?: boolean
 }
 
 const StyledButton = styled.button<StyleProps>`
@@ -20,21 +15,46 @@ const StyledButton = styled.button<StyleProps>`
   border-radius: 12px;
   background: ${(props) => props.active ? "cyan" : "darkcyan"};
   box-shadow: 0 0.125rem 0.25rem rgb(0 0 0 / 8%);
+  color: ${(props) => props.active ? "white" : "gainsboro"};
+  
+  :active {
+    background: cyan;
+    color: white;
+  }
 
-  img {
+  & > * {
     height: 70%;
     width: 70%;
     object-fit: contain;
   }
+
+  .active {
+    color: black;
+    box-shadow: 0 0.125rem 0.25rem rgb(0 0 0 / 50%);
+  }
 `;
 
-export const ToolButton: React.FC<ToggleButtonProps> = ({label, iconUrl, active, callback}) => {
-    console.log(iconUrl);
+
+type ToggleButtonProps = {
+    label: string,
+    icon: React.ReactNode,
+    active?: boolean
+}
+
+export const ToolButton: React.FC<ToggleButtonProps & React.HTMLAttributes<HTMLButtonElement>> = ({
+                                                                                                      label,
+                                                                                                      icon,
+                                                                                                      active,
+                                                                                                      ...rest
+                                                                                                  }) => {
+    const classes = classNames({
+        active: active
+    });
 
     return (
-        <StyledButton title={label} style={{backgroundImage: `url(${iconUrl})`}}
-                      onClick={callback} active={active}>
-            <img src={iconUrl} alt={"Tool Icon"}/>
+        <StyledButton className={classes} title={label} active={active} {...rest}>
+            {/*<img src={iconUrl} alt={"Tool Icon"}/>*/}
+            {icon}
         </StyledButton>
     );
 };
