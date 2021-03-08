cb132d39a3874efad0c0ff069e5bb6843fbcedaf
MaxKayan <maxim.kayander1@gmail.com>
2021-03-08 23:20:57 +0300

    Testing proper panning to the first waypoint when active tour is changed

--- frontend/react-app-ts/src/components/MapEventsController/MapEventsController.tsx
+++ frontend/react-app-ts/src/components/MapEventsController/MapEventsController.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {LeafletEventHandlerFnMap} from "leaflet";
 import {useMapEvents} from "react-leaflet";
 import {WaypointsHook} from "../../hooks/useWaypoints";
@@ -9,8 +9,22 @@ type MapEventsControllerProps = {
     waypointsHook: WaypointsHook
 }
 
-export const MapEventsController: React.FC<MapEventsControllerProps> = ({handlers}) => {
-    useMapEvents(handlers);
+export const MapEventsController: React.FC<MapEventsControllerProps> = ({
+                                                                            handlers,
+                                                                            activeRouteId,
+                                                                            waypointsHook
+                                                                        }) => {
+    const map = useMapEvents(handlers);
+
+    useEffect(() => {
+        console.log("Selected tour route was changed.");
+
+        const firstPoint = waypointsHook.points[0];
+        if (firstPoint) {
+            // map.panTo(firstPoint.latLng);
+        }
+
+    }, [activeRouteId]);
 
     return null;
 };
