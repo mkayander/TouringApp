571e78938524f2b1f148f7a4d714918526fdb9a6
MaxKayan <maxim.kayander1@gmail.com>
2021-03-12 15:46:40 +0300

    Cleanup

--- frontend/react-app-ts/src/api/models/TourRoute.ts
+++ frontend/react-app-ts/src/api/models/TourRoute.ts
@@ -24,18 +24,6 @@ export class TourRoute implements TimestampModel, PackableApiModel {
     waypoints: Waypoint[];
     destinations: Destination[];
 
-    // constructor(args: TourRouteResponse) {
-    //     this.pk = args.pk;
-    //     this.created_at = args.created_at && new Date(args.created_at);
-    //     this.updated_at = args.updated_at && new Date(args.updated_at);
-    //     this.title = args.title;
-    //     this.description = args.description;
-    //     this.image = args.image;
-    //     this.waypoints = args.waypoints?.map(value => new Waypoint(value)) || [];
-    //     this.destinations = args.destinations;
-    // }
-
-
     constructor(pk: number, title: string, description: string, waypoints: Waypoint[], destinations: Destination[],
                 created_at?: Date, updated_at?: Date, image?: URL) {
         this.pk = pk;
@@ -88,11 +76,3 @@ export class TourRoute implements TimestampModel, PackableApiModel {
         );
     }
 }
-
-// export type TourRoute = TimestampModel & {
-//     id: number,
-//     title: string,
-//     description: string,
-//     image: URL,
-//     waypoints?: Waypoint[]
-// }
