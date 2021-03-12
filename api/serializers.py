d22ef39fdb4ea02ce32c3c269f2d78546abc09ec
MaxKayan <maxim.kayander1@gmail.com>
2021-03-12 15:50:00 +0300

    Removed unused update&create methods

--- api/serializers.py
+++ api/serializers.py
@@ -25,50 +25,6 @@ class DestinationSerializer(serializers.ModelSerializer):
                   'updated_at', 'photos']
         nested_list_fields = ['photos']
 
-    def create(self, validated_data):
-        print(validated_data)
-        children_data = {}
-        for field_name in self.Meta.nested_list_fields:
-            children_data.update({field_name: validated_data.pop(field_name)})
-
-        instance = self.Meta.model.objects.create(**validated_data)
-
-        for name, data in children_data:
-            self.get_attribute(name).Meta.model.objects.create(**data)
-
-        # children_data = validated_data.pop(self.nested_list_fields[0])
-        # for child_data in children_data:
-        #     self.model.objects.create(**child_data)
-
-        return instance
-
-    def update(self, instance, validated_data):
-        print(validated_data)
-        children_data = {}
-        for field_name in self.Meta.nested_list_fields:
-            children_data.update({field_name: validated_data.pop(field_name)})
-
-        instance = self.Meta.model.objects.update(**validated_data)
-
-        for name, data in children_data:
-            self.get_attribute(name).Meta.model.objects.update_or_create(**data)
-            print(self.get_attribute(name).Meta)
-
-        # children_data = validated_data.pop(self.nested_list_fields[0])
-        # for child_data in children_data:
-        #     self.model.objects.create(**child_data)
-
-        return instance
-
-    # def update(self, instance, validated_data):
-    #     waypoints_data = validated_data.pop('waypoints')
-    #     route = Route.objects.update(**validated_data)
-    #
-    #     for waypoint_data in waypoints_data:
-    #         Waypoint.objects.update_or_create(route=route, **waypoint_data)
-    #
-    #     return route
-
 
 class RouteSerializer(serializers.ModelSerializer):
     waypoints = WaypointSerializer(many=True, required=False)
@@ -77,45 +33,6 @@ class RouteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Route
         fields = ['pk', 'title', 'description', 'image', 'created_at', 'updated_at', 'waypoints', 'destinations']
-
-    def create(self, validated_data):
-        print(dict(validated_data))
-        waypoints_data = validated_data.pop('waypoints')
-        destinations_data = validated_data.pop('destinations')
-        route = Route.objects.create(**validated_data)
-
-        for waypoint_data in waypoints_data:
-            Waypoint.objects.create(route=route, **waypoint_data)
-
-        for destination_data in destinations_data:
-            Destination.objects.create(route=route, **destination_data)
-
-        return route
-
-    def update(self, instance, validated_data):
-        print(dict(validated_data))
-        waypoints_list = validated_data.pop('waypoints')
-        destinations_list = validated_data.pop('destinations')
-
-        # The default update() implementation but without m2m fields
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-
-        instance.save()
-
-        for waypoint_data in waypoints_list:
-            print("\nWaypoint data: ", dict(waypoint_data))
-            try:
-                pk = waypoint_data.pop('pk')
-            except KeyError:
-                pk = None
-            Waypoint.objects.update_or_create(pk=pk, defaults=waypoint_data)
-
-        for destination_data in destinations_list:
-            pk = destination_data.pop('pk')
-            Destination.objects.update_or_create(pk=pk, defaults=destination_data)
-
-        return instance
 
 
 class RouteListSerializer(serializers.ModelSerializer):
